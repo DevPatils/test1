a5463288aaef1020f4ab9ee987b0f9fedbcc53db
DevPatils <dev.patil1006@gmail.com>
2026-01-10 04:13:08 +0530

    chore: initialize package.json with express dependency

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+console.log("starting server");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is alive 🚀" });
+});
+
+const server = app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
+
+server.on("error", (err) => {
+  console.error("Listen error:", err);
+});
